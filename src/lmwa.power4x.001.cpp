03b0f4f3ba0cb63e27f1229518555f7b0bd31192
[Mike DLC] <mikedlc@gmail.com>
2024-08-31 00:07:51 -0600

    mqtt done

--- src/lmwa.power4x.001.cpp
+++ src/lmwa.power4x.001.cpp
@@ -10,10 +10,14 @@
 //Device & MQTT Information
 const char* ProgramID = "LMWA.p4x.001";
 const char* SensorType = "Boost Pumps";
-const char* mqtt_topic = "MYTOPIC";
-const char* mqtt_unit = "Units";
-const char* mqtt_server_init = "homeassistant.local";
+const char* mqtt_topic = "boostpumps";
+const char* mqtt_unit = "Amps";
+const char* mqtt_server_init = "192.168.30.121";
+const char* mqtt_user = "mqttuser";
+const char* mqtt_password = "Lafayette123!";
 
+//OTA Stuff
+#include <ArduinoOTA.h>
 
 // Mottram Labs ML201185 Stuff for the ESP32 Boards (12-bit A/D)
 #include <ESP32_4CH_CT.h>
@@ -55,10 +59,10 @@ char uptimeTotal[30];
 //#include <ESP8266mDNS.h> // Uncomment for D1 mini ES8266
 //#include <WiFiUdp.h> // Uncomment for D1 Mini ESP8266
 void printWifiStatus();
-//const char *ssid =	"LMWA-PumpHouse";		// cannot be longer than 32 characters!
-//const char *pass =	"ds42396xcr5";		//
-const char *ssid =	"WiFiFoFum";		// cannot be longer than 32 characters!
-const char *pass =	"6316EarlyGlow";		//
+const char *ssid =	"LMWA-PumpHouse";		// cannot be longer than 32 characters!
+const char *password =	"ds42396xcr5";		//
+//const char *ssid =	"WiFiFoFum";		// cannot be longer than 32 characters!
+//const char *pass =	"6316EarlyGlow";		//
 WiFiClient wifi_client;
 String wifistatustoprint;
 
@@ -108,6 +112,96 @@ void setup() {
   display.display(); //Turn on
   delay(2000);
 
+  WiFi.mode(WIFI_STA);
+  if (WiFi.status() != WL_CONNECTED) {
+    
+    //Write wifi connection to display
+    display.setTextSize(1);
+    display.setTextColor(SH110X_WHITE);
+    display.setCursor(0, 0);
+    display.println("Booting Program ID:");
+    display.println(ProgramID);
+    display.println("Sensor Type:");
+    display.println(SensorType);
+    display.println("Connecting To WiFi:");
+    display.println(ssid);
+    display.println("\nWait for it......");
+    display.display();
+
+    //write wifi connection to serial
+    Serial.print("Connecting to ");
+    Serial.print(ssid);
+    Serial.println("...");
+    WiFi.begin(ssid, password);
+    WiFi.setHostname(ProgramID);
+
+    //delay 8 seconds for effect
+    delay(8000);
+
+    if (WiFi.waitForConnectResult() != WL_CONNECTED){
+      return;
+    }
+
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Booting Program ID:");
+    display.println(ProgramID);
+    display.println("Sensor Type:");
+    display.println(SensorType);
+    display.println("Connected To WiFi:");
+    display.println(ssid);
+    display.println(WiFi.localIP());
+    display.display();
+    delay(5000);
+    Serial.println("\n\nWiFi Connected! ");
+  //  printWifiStatus();
+
+  }
+
+//OTA Setup Stuff
+  if(1){
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    display.clearDisplay();
+    Serial.println("Start OTA");
+    display.setCursor(0, 0);
+    display.println("Starting OTA!");
+    display.display();
+    });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd OTA - Rebooting!");
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("OTA Done!"); display.println("Rebooting!");
+    display.display();
+    ESP.restart();
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.print("Progress: " + (progress / (total / 100)));
+    display.display();
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  }//End OTA Code Wrapper
+
+
   //MQTT Setup
   pubsub_client.setServer(mqtt_server, 1883);
   pubsub_client.setCallback(callback);
@@ -135,7 +229,9 @@ void loop() {
   uptimeSeconds=secsRemaining%60;
   sprintf(uptimeTotal,"Uptime %02dD:%02d:%02d:%02d",uptimeDays,uptimeHours,uptimeMinutes,uptimeSeconds);
 
-  //Wifi Stuff
+  ArduinoOTA.handle(); // Start listening for OTA Updates
+
+  /*//Wifi Stuff
   if (WiFi.status() != WL_CONNECTED) {
     
     //Write wifi connection to display
@@ -156,7 +252,7 @@ void loop() {
     Serial.print(ssid);
     Serial.println("...");
     WiFi.setHostname(ProgramID);
-    WiFi.begin(ssid, pass);
+    WiFi.begin(ssid, password);
 
     //delay 8 seconds for effect
     delay(8000);
@@ -187,7 +283,7 @@ void loop() {
   }else{
     wifistatustoprint="Womp, No Wifi!";
   }
-
+*/
 
 
   // read A/D values and store in array Value[]
@@ -211,7 +307,7 @@ void loop() {
   delay(100);
 
   String Report = String(PowerReadings[0]) + ", " + String(PowerReadings[1]) + ", " + String(PowerReadings[2]) + ", " + String(PowerReadings[3]) + "      ";
-  Serial.println(Report);
+  //Serial.println(Report);
 
   display.clearDisplay(); // clear the display
 
@@ -325,7 +421,7 @@ void reconnect() {
     String clientId = "PUMPSENSOR-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
-    if (pubsub_client.connect(clientId.c_str(), "mqttuser", "Quik5ilver7")) {
+    if (pubsub_client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
       Serial.println("connected");
     } else {
       Serial.print("failed, rc=");
@@ -338,8 +434,7 @@ void reconnect() {
 }
 
 void sendMQTT(double PowerReading) {
-  Serial.println("Sending alert via MQTT...");
-  Serial.println(PowerReading);
+
   if (!pubsub_client.connected()) {
     reconnect();
   }
@@ -348,16 +443,26 @@ void sendMQTT(double PowerReading) {
   if (now - lastMsg > 2000) {
     lastMsg = now;
     ++value;
-
+    
+    Serial.println("\nSending alert via MQTT...");
     //msg variable contains JSON string to send to MQTT server
     //snprintf (msg, MSG_BUFFER_SIZE, "\{\"amps\": %4.1f, \"humidity\": %4.1f\}", temperature, humidity);
-    snprintf (msg, MSG_BUFFER_SIZE, "\{\"amps\": %4.2f\}", PowerReading);
-    //Due to a quirk with escaping special characters, if you're using an ESP8266 you will need to use this instead:
-    //snprintf (msg, MSG_BUFFER_SIZE, "{\"temperature\": %4.1f, \"humidity\": %4.1f}", temperature, humidity);
-
-    Serial.print("Publish message: ");
+    
+    snprintf (msg, MSG_BUFFER_SIZE, "{\"Amps\": %4.2f}", PowerReadings[0]);
+    Serial.print("Publish message to 01: ");
     Serial.println(msg);
-    pubsub_client.publish("BOOSTPUMP01", msg);
+    pubsub_client.publish("boostpumps/01", msg);
+
+    snprintf (msg, MSG_BUFFER_SIZE, "{\"Amps\": %4.2f}", PowerReadings[1]);
+    Serial.print("Publish message to 02: ");
+    Serial.println(msg);
+    pubsub_client.publish("boostpumps/02", msg);
+
+    snprintf (msg, MSG_BUFFER_SIZE, "{\"Amps\": %4.2f}", PowerReadings[2]);
+    Serial.print("Publish message to 03: ");
+    Serial.println(msg);
+    pubsub_client.publish("boostpumps/03", msg);
+
   }
 
 }
